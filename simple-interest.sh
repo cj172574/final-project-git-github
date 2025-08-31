6eb458d7eff95a2b0ce09f8a024d3967342ba219
cj172574 <91695446+cj172574@users.noreply.github.com>
2025-08-31 15:49:47 +0530

    Create simple-interest.sh

---
+++ simple-interest.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Simple Interest Calculator Script
+
+# Formula: (Principal * Time * Rate) / 100
+
+echo "Enter Principal:"
+read p
+echo "Enter Time (in years):"
+read t
+echo "Enter Rate of Interest:"
+read r
+
+si=$((p * t * r / 100))
+
+echo "Simple Interest = $si"
